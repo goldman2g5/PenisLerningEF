c8a196b3fabe6730075b886589f6c9f3c99fd375
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-01-12 13:12:02 +0300

    bebra

---
+++ WebApplication5/Models/FormModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml.Linq;
+using static WebApplication5.Models.FormModel;
+
+namespace WebApplication5.Models
+{
+    public class FormModel : PageModel
+    {
+
+        [BindProperty] public string Name { get; set; }
+        [BindProperty] public string Surname { get; set; }
+        [BindProperty] public string Age { get; set; }
+        [BindProperty]
+        public string City { get; set; }
+
+        public void OnPost()
+        {
+            ViewData["sentence"] = $"{Name} {Surname}, {Age} lives in {City}.";
+        }
+
+        public void OnGet()
+        {
+            ViewData["sentence"] = "";
+        }
+    }
+}
