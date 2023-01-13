16470749b6480eb58bee25879e9cc8a1c447194b
Sharp336 <39415926+Sharp336@users.noreply.github.com>
2023-01-13 13:16:21 +0300

    klml;ml

--- WebApplication5/Controllers/CasinoController.cs
+++ WebApplication5/Controllers/CasinoController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace WebApplication5.Controllers
 {
     public class CasinoController : Controller
     {
+
+        [ViewData] public int Count { get; set; }
+
+
+        [HttpGet]
         public IActionResult Index()
         {
+            Count = 1;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int data = 0)
+        {
+            Count++;
+            Console.WriteLine(Count);
             return View();
         }
     }
